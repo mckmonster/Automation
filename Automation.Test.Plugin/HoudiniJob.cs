60506c3e1295cbba00da5ea12f4c004727be6f46
Veaudour Mickael <mckmonster@gmail.com>
2017-10-14 20:46:45 +0200

    Add Map choice

--- Automation.Test.Plugin/HoudiniJob.cs
+++ Automation.Test.Plugin/HoudiniJob.cs
@@ -3,30 +3,46 @@ using YAXLib;
 
 namespace Automation.Test.Plugin
 {
+    public enum WorldEnum
+    {
+        STP_Alpes,
+        STP_Alaska,
+        STP_Japan
+    }
+
     public abstract class HoudiniJob : UbuildJob
     {
         [Browsable(true)]
         [ReadOnly(true)]
         public string JobName { get; set; }
 
-        public string WorldName { get; set; }
+        [YAXErrorIfMissed(YAXExceptionTypes.Ignore, DefaultValue = WorldEnum.STP_Japan)]
+        [DisplayName("World Name")]
+        public WorldEnum WorldName { get; set; }
 
         [YAXErrorIfMissed(YAXExceptionTypes.Ignore, DefaultValue = 0)]
+        [Category("Sync Settings")]
+        [Description("Version of Code needed")]
         public int CodeVersionId { get; set; }
-        
+
         [YAXErrorIfMissed(YAXExceptionTypes.Ignore, DefaultValue = 0)]
+        [Category("Sync Settings")]
+        [Description("Version of BigFile needed")]
         public int BigFileVersionId { get; set; }
 
         [YAXErrorIfMissed(YAXExceptionTypes.Ignore, DefaultValue = 0)]
+        [Category("Sync Settings")]
         [Description("CL Data to create Job")]
         public int Data { get; set; }
 
         [YAXErrorIfMissed(YAXExceptionTypes.Ignore, DefaultValue = 0)]
+        [Category("Sync Settings")]
         [Description("CL Rawdata to create Job")]
         public int Rawdata { get; set; }
 
         public HoudiniJob(string name) : base(name)
         {
+            WorldName = WorldEnum.STP_Japan;
         }
     }
 }
